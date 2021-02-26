a49e3833340c6c26265991f860ad4b95c17e27ac
BrenoFischer <42100477+BrenoFischer@users.noreply.github.com>
2021-02-26 12:46:17 -0300

    JSON enviado a cada 2s, com 2 drones diferentes

--- every2secJSON.ino
+++ every2secJSON.ino
@@ -1,43 +1,38 @@
 #define ARDUINOJSON_USE_DOUBLE 1
 #include <ArduinoJson.h>
 
-String string_test;
 StaticJsonDocument<128> doc;
+int ind;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello World!");
-  
+  ind = 0;
   while (!Serial) continue;
 }
 
 void loop() {
-  while(Serial.available())  
-  {  
-      string_test = Serial.readString( ); 
-      
-      if (string_test.equals("A\n") || string_test.equals("a\n")) {
-        doc["id"] = "1";
-        doc["type"] = "a";
-        doc["lat"] = 56.756080;
-        doc["lng"] = -9.302038;
-        doc["high"] = 10.2;
-        doc["data"] = "teste";
-
-        serializeJson(doc, Serial);
-        Serial.println();
-      } else if (string_test.equals("B\n") || string_test.equals("b\n")) {
-          doc["id"] = "2";
-          doc["type"] = "b";
-          doc["lat"] = 53.2839936;
-          doc["lng"] = -9.1187864;
-          doc["high"] = 11.1;
-          doc["data"] = "teste2";
-
-          serializeJson(doc, Serial);
-          Serial.println();
-      } else {
-        Serial.println("Type Unknown");
-      }
+  delay(10000);
+  if(ind == 0) {
+    doc["id"] = "1";
+    doc["type"] = "a";
+    doc["lat"] = 53.2839936;
+    doc["lng"] = -9.302038;
+    doc["high"] = 10.2;
+    doc["data"] = "teste";
+    serializeJson(doc, Serial);
+    Serial.println();
+    ind = 1;
+  }
+  else {
+    doc["id"] = "2";
+    doc["type"] = "b";
+    doc["lat"] = 52.6515985;
+    doc["lng"] = -8.7001799;
+    doc["high"] = 11.1;
+    doc["data"] = "teste2";
+    serializeJson(doc, Serial);
+    Serial.println();
+    ind = 0;
   }  
 }
